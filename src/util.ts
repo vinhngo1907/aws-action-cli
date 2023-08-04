181ac8235d69d2f03b0bfb21c5214b0f020ba9dc
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-08-04 20:36:45 +0700

    commit

--- src/util.ts
+++ src/util.ts
@@ -4,9 +4,7 @@ export function _filterVersion(stdmsg: string): string {
     const cliVersion = stdmsg.match('(\\d+\\.)(\\d+\\.)(\\d+)');
     if (cliVersion) {
         return cliVersion[0];
-    } else {
-        return '0.0.0';
-    }
+    } else { return '0.0.0'; }
 }
 
 export function _readFile(path: string, usrOpts: object): Promise<string> {
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -25,22 +25,39 @@ function setupTest(): void {
         await rmRF(tempPath);
     });
 
-    afterAll(async function () {
-        await rmRF(tempPath);
-        await rmRF(cachePath);
-    });
+    // afterAll(async function () {
+    //     await rmRF(tempPath);
+    //     await rmRF(cachePath);
+    // });
 
     describe('Test Functions', () => {
         setupTest();
-        it('Will give the tool path if already set', async () => {
-            const downPath: string = await downloadTool(
-                'http://example.com/bytes/35'
-            );
-            await cacheFile(downPath, 'aws', 'aws', '1.1.0');
-            const toolPath: string = find('aws', '1.1.0');
-            const cachedPath = await _installTool();
-            expect(cachedPath).toBe(toolPath);
+        // it('Will give the tool path if already set', async () => {
+        //     const downPath: string = await downloadTool(
+        //         'http://example.com/bytes/35'
+        //     );
+        //     await cacheFile(downPath, 'aws', 'aws', '1.1.0');
+        //     const toolPath: string = find('aws', '1.1.0');
+        //     const cachedPath = await _installTool();
+        //     expect(cachedPath).toBe(toolPath);
+        // });
+        it('Will parse a std(out/err) string and return the version', () => {
+            const msg = 'aws-cli/1.16.220 Python/2.7.10 Darwin/18.2.0 botocore/1.12.210'
+            const expectedVersion = '1.16.220';
+            const msgErr = 'unexpected stdout';
+            const returnedVersion = _filterVersion(msg);
+            const returnedVersionErr = _filterVersion(msgErr);
+            expect(returnedVersion).toEqual(expectedVersion);
+            expect(returnedVersionErr).toEqual('0.0.0');
         });
-
+    });
+    describe('Test End to End', () => {
+        setupTest()
+        const installTimeout = 300000
+        it('Will download, cache and return aws-cli', async () => {
+            jest.setTimeout(installTimeout)
+            const toolPath = await _installTool()
+            expect(toolPath).toContain('aws')
+        }, installTimeout)
     });
 }
